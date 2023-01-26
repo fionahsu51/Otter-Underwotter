681573945073fe7d2d27df2a90e09950de12e8d2
Malachi Maldonado <m.maldonado1792@gmail.com>
2023-01-25 16:31:30 -0800

    bullets fire the direction the player is facing!

---
+++ Assets/Scripts/BulletVelocity.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletVelocity : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * speed;
+    }
+}
