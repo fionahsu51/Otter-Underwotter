386fa6b2a555e30bbcbe8661320d960cbbccc0d3
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-02-03 14:58:55 -0800

    quit function added

--- Assets/Scripts/PauseScene.cs
+++ Assets/Scripts/PauseScene.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PauseScene : MonoBehaviour
 {
+    public GameObject yesButton;
+    public GameObject noButton;
+    public GameObject quitQ;
+    public GameObject pauseBackground;
     bool paused = false;
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        pauseBackground.SetActive(false);
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
+        quitQ.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,16 +25,42 @@ public class PauseScene : MonoBehaviour
         {
             Time.timeScale = 0;
             Cursor.lockState = CursorLockMode.Locked;
+            pauseBackground.SetActive(true);
+            yesButton.SetActive(true);
+            noButton.SetActive(true);
+            quitQ.SetActive(true);
             paused = true;
             Debug.Log("Escape key was pressed");
         }
 
-        else if(Input.GetKeyDown(KeyCode.Escape) && paused == true)
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused == true)
         {
             Time.timeScale = 1;
             Cursor.lockState = CursorLockMode.None;
+            pauseBackground.SetActive(false);
+            yesButton.SetActive(false);
+            noButton.SetActive(false);
+            quitQ.SetActive(false);
             paused = false;
             Debug.Log("Escape key was pressed!");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Y) && paused == true)
+        {
+            Debug.Log("Y key was pressed!");
+            Application.Quit();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.N) && paused == true)
+        {
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.None;
+            pauseBackground.SetActive(false);
+            yesButton.SetActive(false);
+            noButton.SetActive(false);
+            quitQ.SetActive(false);
+            paused = false;
+            Debug.Log("N key was pressed!");
         }
     }
 }
