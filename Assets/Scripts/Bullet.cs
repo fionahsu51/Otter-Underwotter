0e090312df88e30f65fe772a1adb6a530cb6e189
Malachi Maldonado <m.maldonado1792@gmail.com>
2023-02-03 17:27:17 -0800

    health script added

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -30,6 +30,5 @@ public class Bullet : MonoBehaviour
             Destroy(gameObject);
         }
         
-
     }
 }
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+
+    public float health;
+    // Update is called once per frame
+    void Update()
+    {
+        if(health == 0){
+            SceneManager.LoadScene("Title_Screen");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+
+        Debug.Log("collision");
+
+        Enemy enemy = collision.GetComponent<Enemy>();
+
+        if(enemy != null){
+            health -= 10;
+        }
+        
+    }
+}
