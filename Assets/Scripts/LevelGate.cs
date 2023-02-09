b187fa2fd77584a4f35cdb48cc6e93155a23a8ae
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2023-02-08 16:13:56 -0800

    Mal_prototype transitions to Level2_Twilight level

--- Assets/Scripts/LevelGate.cs
+++ Assets/Scripts/LevelGate.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelGate : MonoBehaviour
 {
@@ -23,7 +24,15 @@ void OnCollisionEnter2D(Collision2D collision)
         //Check for a match with the specified name on any GameObject that collides with your GameObject
         if (activated && collision.gameObject.name == "Otter")
         {
-            print("LEVELGATE COLLISION DETECTED");
+            string now =  SceneManager.GetActiveScene().name;
+            string next = "";
+            if (now == "Mal_prototype") next = "Level2_Twilight";
+            else if (now == "Level2_Twilight") next = "Level3_Midnight";
+            else if (now == "Level3_Midnight") next = "Level4_Abyss";
+            else if (now == "Level4_Abyss") next = "Level5_Trench";
+
+            //print("LEVELGATE COLLISION DETECTED");
+            SceneManager.LoadScene(next, LoadSceneMode.Single);
         }
     }
 
