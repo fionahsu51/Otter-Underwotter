0ecc4bce38531e15c4259c30daa71ceae17bcc14
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2023-02-09 14:26:57 -0800

    implemented gradual otter turning

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,10 +9,12 @@ public class Movement : MonoBehaviour
     bool mouseHover = false;
     private float lerpVal = 0f;
     private float accelerationSpeed = 0.11f;
+    public float turnRate;
 
     void Start()
     {
         newPosition = transform.position;
+        turnRate = 550f;
     }
 
     // Update is called once per frame
@@ -44,7 +46,8 @@ public class Movement : MonoBehaviour
         //Debug.Log("Mouse is no longer on GameObject.");
     }
 
-    void faceMouse()
+    //old faceMouse.  instantly snaps to mouse position
+    /*void faceMouse()
     {
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
@@ -53,11 +56,26 @@ public class Movement : MonoBehaviour
         transform.up = direction;
 
         if (lerpVal <= 1f) lerpVal += accelerationSpeed;
+    }*/
+
+    //new faceMouse.  Gradually turns towards mouse position at a rate of turnRate
+    //adapted from:  https://answers.unity.com/questions/826613/how-would-i-slow-down-spinning-toward-the-mouse.html
+    void faceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+
+        float angle = Mathf.Atan2 (mousePosition.y - transform.position.y, mousePosition.x-transform.position.x) * Mathf.Rad2Deg;
+        Quaternion qTo = Quaternion.Euler (new Vector3 (0, 0, angle - 90f));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo, turnRate * Time.deltaTime);
+
+        if (lerpVal <= 1f) lerpVal += accelerationSpeed;
     }
 
     void dash()
     {
-        //Debug.Log("dash");
         transform.position += (transform.up * Time.deltaTime * speed) * lerpVal;
     }
 }
