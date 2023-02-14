88cf961ad2dbeb517d47d90d2aacffb88d129300
Malachi Maldonado <m.maldonado1792@gmail.com>
2023-02-13 17:36:18 -0800

    reworked enemy implementation
    Enemy is now an abstract class. we can create enemy subclasses with their own unique movement scripts now.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,16 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public abstract class Enemy : MonoBehaviour
 {
 
     public float health;
-    public float type;
     public float speed;
     public float leftBound;
     public float rightBound;
-    private Rigidbody2D rb;
-    private Vector3 dir = Vector3.left;
+    protected Rigidbody2D rb;
+    protected Vector3 dir = Vector3.left;
 
     // Start is called before the first frame update
     void Start()
@@ -22,25 +21,6 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //enemy behavior varies depending on type.
-        switch(type){
-            case 0:
-                speed = 3;
-
-                // Code adapted from here. Thank you Zarenityx!
-                // https://answers.unity.com/questions/1558555/moving-an-object-left-and-right.html
-                transform.Translate(dir*speed*Time.deltaTime);
- 
-                if(transform.position.x <= leftBound){
-                    dir = Vector3.right;
-                }else if(transform.position.x >= rightBound){
-                    dir = Vector3.left;
-                }
-                break;
-            default:
-                break;
-        }
-
 
         if(health == 0){
             die();
@@ -55,5 +35,7 @@ public class Enemy : MonoBehaviour
     public void die(){
         Destroy(gameObject);
     }
+
+    public abstract void move();
 
 }
