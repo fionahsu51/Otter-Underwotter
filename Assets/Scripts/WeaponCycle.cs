81b6cd58f208d92c8b4b03408276fbf3216d68ab
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-02-13 20:11:54 -0800

    UI display for weapon switching working

--- Assets/Scripts/WeaponCycle.cs
+++ Assets/Scripts/WeaponCycle.cs
@@ -4,18 +4,59 @@ using UnityEngine;
 
 public class WeaponCycle : MonoBehaviour
 {
-    public string[] weapons;
+
+    //public string[] weaponsArray = new string[] { "slingshot", "shield", "ball" };
+    /// public GameObject[] weaponsDisplayArray = GameObject[] {"slingshotDisplay", "shieldDisplay", "ballDisplay"};
+    //var weaponsList = weaponsArray.ToList();
+    //weaponsList.Add("ball");
+    //weaponsArrary = weaponsList.ToArray();
+
+    //string[] weaponsArray = new string[] { "slingshot", "shield" };
+    //string ball = "ball";
+    //weaponsArray = weaponsArray.Append(ball).ToArray();
+
+    public GameObject[] weaponsDisplay; 
+    public GameObject slingshotDisplay;
+    public GameObject shieldDisplay;
+    public GameObject ballDisplay;
+
+    bool ballObtained = false;
+    int i = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       /// weaponsDisplay = new GameObject[3];
+
+        slingshotDisplay.SetActive(true);
+        shieldDisplay.SetActive(false);
+        ballDisplay.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Input.GetKeyDown(KeyCode.Z) {
+        if (Input.GetKeyDown("Z")){
+            if (i == 0)
+            {
 
-        //}
+            }
+            if (i == 1)
+            {
+                slingshotDisplay.SetActive(false);
+                shieldDisplay.SetActive(true);
+                i++;
+            }
+            else if(ballObtained == false)
+            {
+                i = 0;
+            }
+
+            else if(i == 2 && ballObtained == true)
+            {
+                shieldDisplay.SetActive(false);
+                ballDisplay.SetActive(true);   
+            }
+        }
     }
 }
---
+++ Assets/Scripts/WeaponSwitching.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Script from https://www.youtube.com/watch?v=Dn_BUIVdAPg&ab_channel=Brackeys
+public class WeaponSwitching : MonoBehaviour
+{
+    public int selectedWeapon = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectWeapon();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousSelectedWeapon = selectedWeapon;
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if(selectedWeapon >= transform.childCount - 1)
+            {
+                selectedWeapon = 0;
+            }
+            
+            else
+            {
+                selectedWeapon++;
+            }
+        }
+
+        if(previousSelectedWeapon != selectedWeapon)
+        {
+            SelectWeapon();
+        }
+    }
+    void SelectWeapon()
+    {
+        int i = 0;
+        foreach (Transform weapon in transform)
+        {
+            if(i == selectedWeapon)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
