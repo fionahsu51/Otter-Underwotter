4a747f9536ad85dd2ef3cd668c62a7979103c298
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-02-23 16:53:39 -0800

    changed player camera script to have boundaries

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -6,13 +6,24 @@ public class PlayerCamera : MonoBehaviour
 {
 
     public Transform followTransform;
-
+    public Vector3 minValues, maxValue;
+    public float smoothFactor;
 
     // Update is called once per frame
+
+    // Code adapted from this tutorial https://www.youtube.com/watch?v=Fqht4gyqFbo&ab_channel=Antarsoft 
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        //this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
 
+        Vector3 targetPosition = followTransform.position;
 
+        //Set the camera boundaires to the edges of the background
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minValues.x, maxValue.x),
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValue.y),
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValue.z));
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPosition;
     }
 }
