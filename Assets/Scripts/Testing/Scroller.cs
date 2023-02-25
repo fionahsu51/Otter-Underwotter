2bd8d946c55e8fb7e8780b5125a6464940d0ebf3
Amm0n1te <103222407+Amm0n1te@users.noreply.github.com>
2023-02-24 20:15:04 -0800

    integrated scrolling system into OFFICIAL scene

--- Assets/Scripts/Testing/Scroller.cs
+++ Assets/Scripts/Testing/Scroller.cs
@@ -9,9 +9,15 @@ public class Scroller : MonoBehaviour
     public GameObject otter;
     public SpriteRenderer bg1;
     public SpriteRenderer bg2;
+    public Sprite startSprite;
     public Sprite sunSprite;
-    public Sprite transMidSprite;
-    public Sprite midnightSprite;
+    public Sprite tranSunTwiSprite;
+    public Sprite twiSprite;
+    public Sprite tranTwiMidSprite;
+    public Sprite midSprite;
+    public Sprite tranMidAbSprite;
+    public Sprite abSprite;
+    public Sprite endSprite;
     private float depth;
 
     private float time;
@@ -21,8 +27,12 @@ public class Scroller : MonoBehaviour
     private float offset;
     private float start;
 
-    public int sunlightBound;
-    public int midnightBound;
+    public int sunlightLimit;
+    public int twilightLimit;
+    public int midnightLimit;
+    public int end;
+
+    public GameObject bottom;
 
     // Start is called before the first frame update
     void Start()
@@ -32,10 +42,10 @@ public class Scroller : MonoBehaviour
         //bg1.transform.position = new Vector3(bg1.transform.position.x, -bg1.bounds.size.y/2f, 0f)
         start = bg1.transform.position.y/2f;
 
-        sunlightBound = 4;
-        midnightBound = 10;
-
-        //bg1.sprite = midnightSprite;
+        //teleport the bottom collider to the end scene
+        float bottomPos = ((-end-1) * bg1.bounds.size.y);
+        bottomPos += (6 * bg1.bounds.size.y) / 10;
+        bottom.transform.position = new Vector3(bottom.transform.position.x, bottomPos, bottom.transform.position.z);
         
     }
 
@@ -54,8 +64,6 @@ public class Scroller : MonoBehaviour
         //first, find out whether otter is occupying bg1 or bg2
         SpriteRenderer occupying = null;
         SpriteRenderer other = null;
-        //Debug.Log(otter.transform.position.y.ToString() + " < " + (bg1.transform.position.y + halfLength).ToString());
-        //Debug.Log(otter.transform.position.y.ToString() + " < ")
         if (otter.transform.position.y < bg1.transform.position.y + halfLength && otter.transform.position.y > bg1.transform.position.y - halfLength) {
             occupying = bg1;
             other = bg2;
@@ -66,8 +74,9 @@ public class Scroller : MonoBehaviour
 
 
         //first, detect if otter is in upper half or lower half
-        section = Math.Abs( (int)( (otter.transform.position.y - halfLength) / (occupying.bounds.size.y) ) );
-        float currentTop = (- section * occupying.bounds.size.y) + halfLength;
+        section = Math.Abs( (int)( (otter.transform.position.y - halfLength) / (bg1.bounds.size.y) ) );
+        float currentTop = (- section * bg1.bounds.size.y) + halfLength;
+        occupying.transform.position = new Vector3(0f, -section * bg1.bounds.size.y, 0f);
         string half = "";
         if (otter.transform.position.y >= currentTop - halfLength) {
             half = "upper";
@@ -79,30 +88,51 @@ public class Scroller : MonoBehaviour
         } else {
             other.transform.position = new Vector3(0f, occupying.transform.position.y - occupying.bounds.size.y, 0f);
         }
-
+        
         //next make sure bg1 and bg2 have the right background depending on what depth we're at
         //first, take care of occupied background
-        if (section < sunlightBound) {
+        if (section == 0) {
+            occupying.sprite = startSprite;
+        } else if (section < sunlightLimit) {
             occupying.sprite = sunSprite;
-        } else if (section == sunlightBound) {
-            occupying.sprite = transMidSprite;
-        } else if (section > sunlightBound) {  //&& section < midnightBound
-            occupying.sprite = midnightSprite;
+        } else if (section == sunlightLimit) {
+            occupying.sprite = tranSunTwiSprite;
+        } else if (section < twilightLimit) {  //&& section < midnightBound
+            occupying.sprite = twiSprite;
+        } else if (section == twilightLimit) {
+            occupying.sprite = tranTwiMidSprite;
+        } else if (section < midnightLimit) {
+            occupying.sprite = midSprite;
+        } else if (section == midnightLimit) {
+            occupying.sprite = tranMidAbSprite;
+        } else if (section < end) {
+            occupying.sprite = abSprite;
+        } else if (section == end) {
+            occupying.sprite = endSprite;
         }
 
-        
-        //now take care of other background
-        if (section == sunlightBound - 1 && half == "lower") {
-            other.sprite = transMidSprite;
-        } else if (section == sunlightBound && half == "upper") {
-            other.sprite = sunSprite;
-        } else if (section == sunlightBound && half == "lower") {
-            other.sprite = midnightSprite;
-        } else if (section == sunlightBound + 1 && half == "upper") {
-            other.sprite = transMidSprite;
-        } else if (section == sunlightBound + 1 && half == "lower") {
-            other.sprite = midnightSprite;
+
+        if (half == "upper") {
+            if (section == 1) other.sprite = startSprite;
+            else if (section <= sunlightLimit) other.sprite = sunSprite;
+            else if (section == sunlightLimit + 1) other.sprite = tranSunTwiSprite;
+            else if (section <= twilightLimit) other.sprite = twiSprite;
+            else if (section == twilightLimit + 1) other.sprite = tranTwiMidSprite;
+            else if (section <= midnightLimit) other.sprite = midSprite;
+            else if (section == midnightLimit + 1) other.sprite = tranMidAbSprite;
+            else if (section <= end) other.sprite = abSprite;
         }
+        else if (half == "lower") {
+            if (section < sunlightLimit - 1) other.sprite = sunSprite;
+            else if (section == sunlightLimit - 1) other.sprite = tranSunTwiSprite;
+            else if (section < twilightLimit - 1) other.sprite = twiSprite;
+            else if (section == twilightLimit - 1) other.sprite = tranTwiMidSprite;
+            else if (section < midnightLimit - 1) other.sprite = midSprite;
+            else if (section == midnightLimit - 1) other.sprite = tranMidAbSprite;
+            else if (section < end - 1) other.sprite = abSprite;
+            else if (section == end - 1) other.sprite = endSprite;
+        }
+
 
    }
 }
