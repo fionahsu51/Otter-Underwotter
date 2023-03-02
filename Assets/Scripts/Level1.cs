b55827112cdda54b7284552ee7b9c97f2729c306
Malachi Maldonado <m.maldonado1792@gmail.com>
2023-03-01 19:09:10 -0800

    built game again (3/1)

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -54,7 +54,7 @@ public class Level1 : MonoBehaviour
             
             side *= -1;
             enemiesToSpawn--;
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(3.0f);
         }   
     }
 }
