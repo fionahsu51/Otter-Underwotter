1ef17c315e5b2297fcf1137cf9f6db15f9e20bdc
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-03-13 19:22:30 -0700

    shotgun cooldown work in progress

--- Assets/Scripts/AlternateShoot.cs
+++ Assets/Scripts/AlternateShoot.cs
@@ -10,6 +10,8 @@ public class AlternateShoot : MonoBehaviour
     public GameObject bulletPrefab;
     public float pressedSpace;
     public AudioSource shotgunShootSFX;
+    //private bool shootDisabled = false;
+    //private Coroutine buttonDisabled = null;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +21,21 @@ public class AlternateShoot : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
         bool input = Input.GetKeyDown("space");
-        ///bool input2 = Mouse.current.rightButton.wasPressedThisFrame;
+        ///bool input2 = Mouse.current.rightButton.wasPressedThisFrame;     
+
+        //if(buttonDisabled == null)
+        //{
+        //    buttonDisabled = StartCoroutine("ShootCooldown");
+        //}
+
         if(Input.GetKeyDown(KeyCode.X) && Time.timeScale > 0)
         {
             shotgunShootSFX.Play();
             Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
             Instantiate(bulletPrefab, shootingPoint2.position, transform.rotation);
+            //buttonDisabled = StartCoroutine("ShootCooldown");
         }
 
         if (input) 
@@ -35,4 +44,11 @@ public class AlternateShoot : MonoBehaviour
         }
         if (pressedSpace > 0) pressedSpace -= 0.1f;
     }
+
+    //IEnumerator ShootCooldown()
+    //{
+    //    yield return new WaitForSeconds(1.2f);
+    //    shootDisabled = false;
+    //    buttonDisabled = null;
+    //}
 }
