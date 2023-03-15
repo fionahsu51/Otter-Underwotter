92f1d8555f0901a75df6d4429a49127942a84d12
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-03-14 18:40:57 -0700

    added bob to game over buttons

---
+++ Assets/Scripts/Bob.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bob : MonoBehaviour
+{
+    public float amp;
+    public float freq;
+    Vector3 initPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(initPos.x, Mathf.Sin(Time.time * freq) * amp + initPos.y, 0);
+    }
+}
