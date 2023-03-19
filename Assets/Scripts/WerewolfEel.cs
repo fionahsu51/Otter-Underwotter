3a9e9c7fd3e1df11adacceaffee7c1865adcd6af
fionahsu51 <82433180+fionahsu51@users.noreply.github.com>
2023-03-18 19:34:08 -0700

    adjusted volume for some audio

--- Assets/Scripts/WerewolfEel.cs
+++ Assets/Scripts/WerewolfEel.cs
@@ -19,17 +19,22 @@ public class WerewolfEel : Enemy
     // Update is called once per frame
     void Update()
     {
-        if(status != 1){
+        if (status != 1)
+        {
             move();
         }
         //Debug.Log(dashing);
-        if(dashing > 0){
+        if (dashing > 0)
+        {
             speed = 12;  //default was 20
-        }else{
+        }
+        else
+        {
             speed = 0;
         }
 
-        if(health <= 0){
+        if (health <= 0)
+        {
             deathSFX.Play();
             die();
         }
@@ -37,31 +42,38 @@ public class WerewolfEel : Enemy
         if (Vector3.Distance(GameObject.Find("Otter").transform.position, transform.position) > 20f) Destroy(gameObject);
     }
 
-    public override void move(){
+    public override void move()
+    {
         GameObject otter = GameObject.Find("Otter");
         //Rotate eel towards player.
         //totally stole this code from casey. thank you casey! -malachi
-        if(dashing < 0){
-            float angle = Mathf.Atan2 (otter.transform.position.y - transform.position.y, otter.transform.position.x-transform.position.x) * Mathf.Rad2Deg;
-            Quaternion qTo = Quaternion.Euler (new Vector3 (0, 0, angle + 180f));
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo, 
+        if (dashing < 0)
+        {
+            float angle = Mathf.Atan2(otter.transform.position.y - transform.position.y, otter.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion qTo = Quaternion.Euler(new Vector3(0, 0, angle + 180f));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, qTo,
                                     Time.deltaTime * turnRate);
 
-            if((angle > 90f && angle < 180f) || (angle < -90f && angle > -180f)){
-                sprite.flipY = false;    
-            }else{
+            if ((angle > 90f && angle < 180f) || (angle < -90f && angle > -180f))
+            {
+                sprite.flipY = false;
+            }
+            else
+            {
                 sprite.flipY = true;
             }
         }
-        transform.Translate(Vector3.left*speed*Time.deltaTime);  
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
     }
 
-    IEnumerator Dash() {
-        for(;;){
+    IEnumerator Dash()
+    {
+        for (; ; )
+        {
             dashing *= -1;
             //Debug.Log("coroutine called");
             //Debug.Log(dashing);
             yield return new WaitForSeconds(1.5f);
-        } 
+        }
     }
 }
